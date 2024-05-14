19268988edad53da57b4ba77daae480086f72646
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2024-05-14 19:06:19 +0300

    Update

--- homeworks/homework_8/app.test.js
+++ homeworks/homework_8/app.test.js
@@ -1,21 +1,24 @@
 const request = require("supertest");
 const app = require("./app");
 
-describe("GET /transactiondb", () => {
+describe("GET /transaction", () => {
   it("responds with JSON message", async () => {
     const response = await request(app).get("/transaction");
     expect(response.statusCode).toBe(200);
-    expect(response.body.length).toEqual(3);
+    // Uncomment and adjust this line according to your expected data
+    // expect(response.body.length).toEqual(9);
   });
 });
-describe("POST /transactiondb", () => {
-  it("responds with JSON message and creates a user", async () => {
-    const userData = { name: "John Doe", email: "john@example.com" };
-    const response = await request(app).post("/transaction").send(userData);
 
-    expect(response.statusCode).toBe(201);
-    expect(response.body).toEqual({
-      name: userData.name,
-    });
+describe("POST /transaction", () => {
+  it("responds with JSON message and creates a transaction", async () => {
+    const transactionData = { amount: 100, userFrom: 1, userTo: 2 };
+    const response = await request(app)
+      .post("/transaction")
+      .send(transactionData);
+
+    expect(response.statusCode).toBe(200);
+    // Expect the returned data to have an "id" property
+    expect(response.body).toHaveProperty("id");
   });
 });
