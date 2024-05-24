dd2d09117da67019e9c90d4b5c36cebd5f1a3e79
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2024-05-24 08:26:47 +0300

    Update

--- homeworks/homework_12/tic-tac-toe/cypress/e2e/spec-copy-3.cy.js
+++ homeworks/homework_12/tic-tac-toe/cypress/e2e/spec-copy-3.cy.js
@@ -1,25 +1,25 @@
-describe('Tic-Tac-Toe App', () => {
-  beforeEach(() => {
-    cy.visit('http://localhost:5173'); // Adjust the URL as needed
-  });
+// describe('Tic-Tac-Toe App', () => {
+//   beforeEach(() => {
+//     cy.visit('http://localhost:5173'); // Adjust the URL as needed
+//   });
 
-  it('should load the game board', () => {
-    cy.get('.board-row .square').should('have.length', 9);
-  });
+//   it('should load the game board', () => {
+//     cy.get('.board-row .square').should('have.length', 9);
+//   });
 
-  it('should allow players to make moves', () => {
-    cy.get('.board-row .square').eq(0).click().should('contain.text', 'X');
-    cy.get('.board-row .square').eq(1).click().should('contain.text', 'O');
-  });
+//   it('should allow players to make moves', () => {
+//     cy.get('.board-row .square').eq(0).click().should('contain.text', 'X');
+//     cy.get('.board-row .square').eq(1).click().should('contain.text', 'O');
+//   });
 
-  it('should detect a win for player X', () => {
-    [0, 3, 1, 4, 2].forEach(i => cy.get('.board-row .square').eq(i).click());
-    cy.contains('div', 'Winner: X')
-      .should('exist'); // Asserts that the element exists
-  });
+//   it('should detect a win for player X', () => {
+//     [0, 3, 1, 4, 2].forEach(i => cy.get('.board-row .square').eq(i).click());
+//     cy.contains('div', 'Winner: X')
+//       .should('exist'); // Asserts that the element exists
+//   });
 
 
-});
+// });
 
 // <reference types="cypress" />
 
